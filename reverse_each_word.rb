d279d6288bf0b9ac4e37d7440da13322c9b27971
Le H Ngo <seiji@o2.pl>
2019-05-21 13:38:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,12 @@
 # end
 
 def reverse_each_word(string)
-  string.split.collect do |str|
-    string.push(str.reverse)
+  
+
+  str_n = string.split.collect do |str|
+    str.reverse
   end
+
+  str_n.join(" ")
+  
 end
